e6439b29b8f7c56d9df879ee77a92edb10d1aef1
Zarhan Memon <ironmanred118@gmail.com>
2026-09-08 13:20:51 +0530

    os exp 4 producer consumer and read writer poblem

---
+++ OS/exp4_pc_.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+
+#include<pthread.h>
+#include<semaphore.h>
+
+
+int buffer[5];
+
+int front = 0 , rear = 0;
+
+sem_t empty  , full ;
+
+pthread_mutex_t mutex;
+
+
+void *producer ( void *arg){
+
+    for(int i=1; i<= 10 ; i++){
+
+        sem_wait(&empty);
+
+        pthread_mutex_lock(&mutex);
+
+        buffer[front] = i;
+        printf("Produced : %d \n" , buffer[front]);
+        front = (front + 1)%5;
+
+        pthread_mutex_unlock(&mutex);
+
+        sem_post(&full);
+    }
+
+    return NULL;
+}
+
+
+void *consumer( void *arg){
+
+    for( int i=1; i<=10 ; i++){
+
+        sem_wait(&full);
+
+        pthread_mutex_lock(&mutex);
+
+        printf("Consumed: %d \n" , buffer[rear]);
+        rear = (rear+1) % 5;
+
+        pthread_mutex_unlock(&mutex);
+
+        sem_post(&empty);
+
+    }
+    return NULL;
+}
+
+
+int main(){
+
+    pthread_t p , c;
+
+
+    sem_init(&empty , 0 , 5);
+    sem_init(&full , 0 , 0);
+
+    pthread_mutex_init(&mutex , NULL);
+
+
+    pthread_create(&p , NULL , producer , NULL);
+    pthread_create(&c , NULL , consumer , NULL);
+
+    pthread_join(p , NULL);
+    pthread_join(c , NULL);
+
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
+
+
+// Compile-Run:
+// gcc producer_consumer.c -o pc -pthread
+// ./pc
+
+// Produced : 1 
+// Produced : 2 
+// Produced : 3 
+// Produced : 4 
+// Produced : 5 
+// Consumed: 1 
+// Consumed: 2 
+// Consumed: 3 
+// Consumed: 4 
+// Consumed: 5 
+// Produced : 6 
+// Produced : 7 
+// Produced : 8 
+// Produced : 9 
+// Produced : 10 
+// Consumed: 6 
+// Consumed: 7 
+// Consumed: 8 
+// Consumed: 9 
+// Consumed: 10 
+
---
+++ OS/exp4_rw_.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+
+#include<pthread.h>
+
+
+int data = 0;
+
+int read_count = 0;
+
+
+pthread_mutex_t r_mutex;
+
+pthread_mutex_t doc_lock;
+
+
+
+void *reader ( void *arg){
+
+    int id = *(int *)arg;  // for the reader id no
+
+
+    pthread_mutex_lock(&r_mutex);
+
+    read_count++;
+
+    if( read_count == 1){
+        pthread_mutex_lock(&doc_lock);
+    }
+    pthread_mutex_unlock(&r_mutex);
+
+
+
+// CRITICAL SECTION
+    printf("Reader %d reads data = %d\n", id, data);
+
+
+    pthread_mutex_lock(&r_mutex);
+
+    read_count--;
+
+    if(read_count ==0){
+        pthread_mutex_unlock(&doc_lock);
+    }
+
+    pthread_mutex_unlock(&r_mutex);
+
+
+   return NULL; 
+}
+
+
+
+void *writer( void *arg){
+
+    int id = *(int *)arg;  // FOR THE id of the write
+
+
+    pthread_mutex_lock(&doc_lock);
+
+    data = data + 1;    // critical section for editing the doc data
+
+    printf("Writer %d writes data = %d\n", id, data);
+
+    pthread_mutex_unlock(&doc_lock);
+
+
+    return NULL;
+}
+
+
+
+int main(){
+
+    pthread_t r[3] , w[2];
+
+    int rid[3] = {1 ,2 ,3};
+    int wid[2] = {1 ,2} ;
+
+
+    pthread_mutex_init(&r_mutex , NULL);
+    pthread_mutex_init(&doc_lock , NULL);
+
+    
+    for( int i=0 ; i<3 ; i++){
+        pthread_create(&r[i] , NULL , reader , &rid[i]);
+    }
+
+    for( int i=0; i<2 ; i++){
+        pthread_create(&w[i] , NULL , writer , &wid[i]);
+    }
+
+
+
+    for( int i=0; i<3 ; i++){
+        pthread_join(r[i] , NULL );
+    }
+
+    for( int i=0 ; i<2 ; i++){
+        pthread_join(w[i] , NULL);
+    }
+
+
+    return 0;
+}
+
+
+
+//RUN :
+// gcc rw.c -o rw -pthread
+// ./rw
+
+
+// Reader 1 reads data = 0
+// Reader 2 reads data = 0
+// Reader 3 reads data = 0
+// Writer 1 writes data = 1
+// Writer 2 writes data = 2
